13f726d48740d40a548ee5a2a8b57ece92e325db
Israel Morales <israel.morales@rocket.com.mx>
2016-03-21 17:38:15 -0700

    burger icon

---
+++ js/custom.js
@@ -0,0 +1,14 @@
+(function ($) {
+    'use strict';
+    Drupal.behaviors.ablog = {
+        attach: function (context, settings) {
+            $('.fa-bars', context).bind('click', function() {
+                if($('html').hasClass('show')) {
+                    $('html').removeClass('show').addClass('hide');
+                } else {
+                    $('html').addClass('show').removeClass('hide');
+                }
+            });
+        }
+    };
+})(jQuery);
