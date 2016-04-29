58b51264c3d38dee084ecc16d1898213d93790b0
Israel Morales <israel.morales@rocket.com.mx>
2016-04-29 13:55:01 -0700

    Responsive - full cover for xs devices main block

--- js/custom.js
+++ js/custom.js
@@ -2,13 +2,7 @@
     'use strict';
     Drupal.behaviors.ablog = {
         attach: function (context, settings) {
-            // $('.fa-bars', context).bind('click', function() {
-            //     if($('html').hasClass('show')) {
-            //         $('html').removeClass('show').addClass('hide');
-            //     } else {
-            //         $('html').addClass('show').removeClass('hide');
-            //     }
-            // });
+
         }
     };
 })(jQuery);
